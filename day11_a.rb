2c0b01fd86501bb4c62feea035e085aed83dbb07
Animesh Sahu <animeshsahu19@yahoo.com>
2023-12-11 12:04:27 +0530

    feat: day 11 (a + b) in ruby

---
+++ day11_a.rb
@@ -0,0 +1,21 @@
+lines = []
+while line=gets&.chomp
+  lines << line
+end
+lines.map!(&:chars)
+
+rows_inc = lines.each_with_object([]) { |row, acc| acc << (acc.last || 0) + (row.all? { |c| c == '.' } ? 1 : 0) }
+cols_inc = lines.transpose.each_with_object([]) { |col, acc| acc << (acc.last || 0) + (col.all? { |c| c == '.' } ? 1 : 0) }
+
+coordinates = []
+lines.each_with_index do |line, row|
+  line.each_with_index do |char, col|
+    coordinates << [row, col] if char == '#'
+  end
+end
+
+min_dists = coordinates.combination(2).map { |a, b|
+  (b[1]-a[1]).abs + (b[0]-a[0]).abs + (rows_inc[b[0]]-rows_inc[a[0]]).abs + (cols_inc[b[1]]-cols_inc[a[1]]).abs
+}
+
+puts min_dists.sum
